d503ad479d4b2fa26a882be14aed6d6a48cd98bd
David Silva <davidlsilva.sp@gmail.com>
2025-07-28 13:41:25 -0300

    feat: add tailwind

--- front-end/src/main.tsx
+++ front-end/src/main.tsx
@@ -1,6 +1,9 @@
 import ReactDOM from 'react-dom/client';
+import './index.css';
 
-const App = () => <h1>Hello, World!!</h1>;
+const App = () => <>
+  <h1 className='text-3xl font-bold underline'>Hello, World!!</h1>
+</>;''
 
 const root = ReactDOM.createRoot(document.getElementById('root') as HTMLElement);
 root.render(<App />);
